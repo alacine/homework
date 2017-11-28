e447dfafa8bfeac2edc426c4cc83e7a00bc3938a
RyanAlacine <ryanruirr@gmail.com>
2017-11-28 13:34:16 +0800

    maze example

--- Data-Structure/maze/maze.cpp
+++ Data-Structure/maze/maze.cpp
@@ -1,37 +1,133 @@
-#include <cstdio>
+#include <iostream>
+#include <cstring>
 #include <stack>
+#include <cstdio>
+
 using namespace std;
+int w, h;
 
-const int maxn = 1010;
-int n, target[maxn];
+const int maxn = 1000;
 
-int main(int argc, char const *argv[])
-{
-    while(scanf("%d",&n) == 1) {
-        stack<int> s;
-        int a = 1, b = 1;
-        for (int i = 1; i <= n; i++) {
-            scanf("%d",&target[i]);
+int maze[maxn][maxn];
+
+struct node {
+    int x,y;//1,2,3,4 下 左 上 右
+};
+node op, ed;
+
+stack <node> sk;
+stack <node> tmp;
+
+bool check(const node& next) {
+    if(next.x < w && next.x >= 0 &&
+       next.y < h && next.y >= 0 &&
+       maze[next.y][next.x] == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool path() {
+    node cur; cur.x = op.x; cur.y = op.y;
+    node next;
+    //将起点入栈
+    sk.push(cur);
+    while(!sk.empty()) {
+        cur = sk.top();
+        //判断是否到了终点
+        if(cur.x == ed.x && cur.y == ed.y)return true;
+        //将该点标记为已经访问
+        maze[cur.y][cur.x] = -1;
+        //向下运动
+        next = cur;
+        next.y++;
+        if(check(next)) {
+            cur = next;
+            sk.push(cur);
+            continue;
         }
-        int ok = 1;
-        while(b <= n) {
-            if(a == target[b]) {
-                a++;
-                b++;
-            }
-            else  if(!s.empty() && s.top() == target[b]) {
-                s.pop();
-                b++;
-            }
-            else if(a <= n) {
-                s.push(a++);
-            }
-            else {
-                ok = 0;
-                break;
-            }
+        //向左运动
+        next = cur;
+        next.x--;
+        if(check(next)) {
+            cur = next;
+            sk.push(cur);
+            continue;
         }
-        printf("%s\n", ok ? "Yes" : "No");
+        //向上运动
+        next = cur;
+        next.y--;
+        if(check(next)) {
+            cur = next;
+            sk.push(cur);
+            continue;
+        }
+        //向右运动
+        next = cur;
+        next.x++;
+        if(check(next)) {
+            cur = next;
+            sk.push(cur);
+            continue;
+        }
+//        cur = sk.top();
+        sk.pop();
+//      maze[cur.x][cur.y] = 0;
+    }
+    return false;
+}
+
+void print() {
+    for(int i = 0; i < h ; i++) {
+        for(int j = 0 ; j < w ; j++) {
+            cout << maze[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    freopen("in.txt","r",stdin);
+    cin >> w >> h;
+
+    memset(maze,0,sizeof(maze));
+
+    for(int i = 0 ; i < h ; i++) {
+        for(int j = 0 ; j < w ; j++) {
+            int tt;
+            cin >> tt;
+            if(tt == 3) {
+                op.y = i;
+                op.x = j;
+                maze[i][j] = 0;
+//                cout << op.x << " " << op.y << endl;
+                continue;
+            }
+            if(tt == 4) {
+                ed.y = i;
+                ed.x = j;
+                maze[i][j] = 0;
+//                cout << ed.x << " " << ed.y << endl;
+                continue;
+            }
+            maze[i][j] = tt;
+        }
+    }
+    if(path()) {
+        while(!sk.empty()) {
+            node t = sk.top();
+            tmp.push(t);
+            sk.pop();
+//            cout << t.x << " " << t.y << endl;
+        }
+        while(!tmp.empty()) {
+            node t = tmp.top();
+            tmp.pop();
+            cout << t.x << " " << t.y << endl;
+        }
+    }
+    else {
+        cout << "++_++" << endl;
     }
     return 0;
 }
