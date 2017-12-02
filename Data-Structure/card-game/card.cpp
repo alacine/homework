1674ff2c03558f0436bf49e5298b90ee8885898f
RyanAlacine <ryanruirr@gmail.com>
2017-12-02 16:51:04 +0800

    add ch04

--- Data-Structure/card-game/card.cpp
+++ Data-Structure/card-game/card.cpp
@@ -5,14 +5,14 @@ const int maxn = 55;
 int card[maxn];
 
 int main() {
-    for (int i = 2; i <= 52; i++) {
+    for(int i = 2; i <= 52; i++) {
         for(int j = i; j <= 52; j += i) {
             card[j] = !card[j];
             //printf("turn %d\n", j);
         }
     }
     card[53] = 1;
-    for (int i = 1; i <= 52 ; i++) {
+    for(int i = 1; i <= 52 ; i++) {
         if(!card[i]) {
             printf("%d ", i);
         }
