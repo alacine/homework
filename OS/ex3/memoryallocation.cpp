53e2ff3b240c242bcd5f217aa309a8a93c9e8940
RyanAlacine <ryanruirr@gmail.com>
2018-05-09 23:06:24 +0800

    update OS

--- OS/ex3/memoryallocation.cpp
+++ OS/ex3/memoryallocation.cpp
@@ -1,4 +1,6 @@
-// 内存动态分区分配
+/* 内存动态分区分配
+ * 采用首次适应算法
+ */
 #include <iostream>
 #include <ctime>
 
@@ -37,14 +39,14 @@ void update_table(Table *start, Table *end) {  // 更新空闲分区表
     for (int i = 1023; i >= 0; i--) {  // 从后往前找
         if (memory[i] == 0) {
             int cnt = 0;
-            for (int j = i; j >= 0; j--) {
+            for (int j = i; j >= 0; j--) {  // 找到了空闲的块，开始计算大小
                 if (memory[j] == 0) {
                     cnt++;
                     continue;
                 }
                 break;
             }
-            Table *ta;
+            Table *ta;  // 在分区表head后插入这个空闲的块
             ta = new Table;
             ta->address = i - cnt + 1;
             ta->size = cnt;
@@ -56,15 +58,18 @@ void update_table(Table *start, Table *end) {  // 更新空闲分区表
 }
 
 int allocate_memory(int n, Table *start, Table *end) {  // 分配内存，分配成功返回1，分配失败返回0
+    for (int i = 0; i < 1024; i++) {  // 已经在内存里的任务不需要再分配
+        if (memory[i] == n + 1) {
+            return 0;
+        }
+    }
     Table *ta;
     ta = start->next;
     while (ta->next != NULL) {
-        if (ta->size >= work[n].size) {
+        if (ta->size >= work[n].size) {  // 找到一个足够大的空闲的块，将该作业载入内存
             for (int i = ta->address; i < ta->address + work[n].size; i++) {
                 memory[i] = n + 1;
             }
-            // ta->address += work[n].size;
-            // ta->size -= work[n].size;
             timetable[n] = clock();  // 记录下该作业开始的时间
             return 1;
         }
@@ -75,8 +80,9 @@ int allocate_memory(int n, Table *start, Table *end) {  // 分配内存，分配
 
 void recovery() {  // 回收运行完成的作业
     clock_t t;
-    t = clock();
+    t = clock();  // 记下开始执行回收操作时的时间
     for (int i = 0; i < 10; i++) {
+        // 当时间已经满足了某一个作业的完成时间，释放该作业占用的内存空间
         if (timetable[i] != 0 && t-timetable[i] >= CLOCKS_PER_SEC*work[i].time) {
             for (int j = 0; j < 1024; j++) {
                 if (memory[j] == i + 1) {
@@ -93,8 +99,8 @@ void print_table(Table *start, Table *end) {  // 打印空闲分区表
     int i = 0;
     cout << "\n" << "Start of Table" << endl;
     while (ta->next != NULL) {
-        cout << i << endl;
-        cout << ta->address << endl;
+        cout << i << " ";
+        cout << ta->address << " ";
         cout << ta->size << endl;
         i++;
         ta = ta->next;
@@ -102,7 +108,7 @@ void print_table(Table *start, Table *end) {  // 打印空闲分区表
     cout << "End of Table\n" << endl;
 }
 
-void show_memory() {
+void show_memory() {  // 显示当前内存空间的分配状况
     for (int i = 0; i < 1024; i++) {
         if (memory[i] == 0) {
             cout << "/" << " ";
@@ -110,11 +116,17 @@ void show_memory() {
         else {
             cout << memory[i]-1 << " ";
         }
-        if ((i + 1) % 50 == 0 && i > 9) {
+        if ((i + 1) % 50 == 0 && i > 9) {  // 方便阅读，一排显示50个内存单元
             cout << endl;
         }
     }
     cout << endl;
+}
+
+void delete_table(Table *start, Table *end) {  // 删除表，释放所有空间
+    clean_table(start, end);
+    delete start;
+    delete end;
 }
 
 int main() {
@@ -126,8 +138,6 @@ int main() {
     update_table(head, end);
     int i;
     while (1) {
-        recovery();
-        update_table(head, end);
         cout << "input the order number (0 <= i <= 9) of work" << endl;
         cout << "quit if i = -1" << endl;
         cin >> i;
@@ -135,23 +145,23 @@ int main() {
             break;
         }
         else {
-            cout << "input the size of this work: ";
-            cin >> work[i].size;
-            cout << "input the time of this work: ";
-            cin >> work[i].time;
+            // 输入作业的大小和需要的时间，以秒为单位
+            cout << "input the size and time of this work: ";
+            cin >> work[i].size >> work[i].time;
+            recovery();  // 回收已完成的作业占用的内存
+            update_table(head, end);  // 回收后及时更新空闲分区表
             if (allocate_memory(i, head, end)) {
                 cout << "SUCCESS" << endl;
-                update_table(head, end);
+                update_table(head, end);  //分配后及时更新空闲分区表
                 print_table(head, end);
             }
             else {
                 cout << "FAILED" << endl;
+                print_table(head, end);
                 show_memory();
-                recovery();
-                update_table(head, end);
             }
         }
     }
-
+    delete_table(head, end); // 程序结束，回收所有空间
     return 0;
 }
