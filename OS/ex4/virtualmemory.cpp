f652981f1eb9e3667f1bf0491217683e4b58c74a
RyanAlacine <ryanruirr@gmail.com>
2018-05-15 00:56:38 +0800

    update OS

--- OS/ex4/virtualmemory.cpp
+++ OS/ex4/virtualmemory.cpp
@@ -12,7 +12,6 @@ const int maxn = 30;
 
 int page[maxn];
 int block[maxn];
-queue<int> block_queue;
 
 
 void init() {  //随机生成一个页面走向的序列
@@ -23,7 +22,7 @@ void init() {  //随机生成一个页面走向的序列
     cout << endl;
 }
 
-bool check(int page, int block_num) {
+bool check(int page, int block_num) {  // 查看当前页面是否在内存中
     for (int i = 0; i < block_num; i++) {
         if (page == block[i]) {
             return true;
@@ -32,7 +31,7 @@ bool check(int page, int block_num) {
     return false;
 }
 
-void show_block(int block_num, int page_index) {
+void show_block(int block_num, int page_index) {  // 打印物理块
     cout << page[page_index] << ": ";
     for (int i = 0; i < block_num; i++) {
         cout << block[i] << " ";
@@ -40,16 +39,19 @@ void show_block(int block_num, int page_index) {
     cout << endl;
 }
 
-void change_block(int block_num, int old, int fresh) {
+int change_block(int block_num, int old, int fresh) {  // 修改物理块
     for (int i = 0; i < block_num; i++) {
         if (block[i] == old) {
             block[i] = fresh;
-            break;
+            return 1;
         }
     }
+    return 0;
 }
 
-void fifo(int block_num) {
+int fifo(int block_num) {  // 先进先出算法
+    queue<int> block_queue;
+    int interruptions_num = 0;
     for (int i = 0; i < block_num; i++) {
         block_queue.push(0);
     }
@@ -59,16 +61,40 @@ void fifo(int block_num) {
             continue;
         }
         block_queue.push(page[i]);
-        change_block(block_num, block_queue.front(), page[i]);
+        interruptions_num += change_block(block_num, block_queue.front(), page[i]);
         if (block_queue.size() >= block_num) {
             block_queue.pop();
         }
         show_block(block_num, i);
     }
+    return interruptions_num;
 }
 
-void opt() {
-    
+int opt(int block_num) {
+    int interruptions_num = 0;
+    for (int i = 0; i < maxn; i++) {
+        int sum = 9999;
+        int index_j = 0;
+        for (int j = 0; j < block_num; j++) {
+            int cnt = 0;
+            for (int k = 0; k < maxn; k++) {
+                if (block[j] == page[k]) {
+                    cnt ++;
+                }
+            }
+            if (cnt < sum) {
+                sum = cnt;
+                index_j = j;
+            }
+        }
+        if (check(page[i], block_num)) {
+            show_block(block_num, i);
+            continue;
+        }
+        interruptions_num += change_block(block_num, block[index_j], page[i]);
+        show_block(block_num, i);
+    }
+    return interruptions_num;
 }
 
 void lru() {
@@ -81,6 +107,10 @@ int main() {
     int n;
     cout << "input the number of blocks" << endl;
     cin >> n;
-    fifo(n);
+    int fifo_ans = fifo(n);
+    cout << "When use FIFO, the number of interruptions is " << fifo_ans << "." << endl;
+    cout << "When use FIFO, the number of page changing is " << fifo_ans + n << "." << endl;
+    cout << "And the page fault rate is " << fifo_ans*100.0/maxn << "\%." << endl;
+    int opt_ans = opt(n);
     return 0;
 }
