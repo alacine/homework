0bd200065608aa53d3d7f65f6fffd16ed74ec26d
RyanAlacine <ryanruirr@gmail.com>
2018-05-21 20:38:04 +0800

    add Newton and Romberg  in Numerical_Analysis

---
+++ Numerical_Analysis/Newton.py
@@ -0,0 +1,44 @@
+# 计算差商
+def get_diff_quo(k, x, fx, c):
+    for i in range(0, k):
+        c.append([])
+        c[i].append(fx[i])
+    for j in range(1, k):
+        for i in range(j, k):
+            c[i].append((c[i-1][j-1] - c[i][j-1]) / (x[i-j] - x[i]))
+            #        开始于首节点的次阶差商 - 结束于尾节点的次阶差商
+            # 差商 = --------------------------------------------
+            #                      首节点 - 尾节点
+
+# 计算牛顿插值多项式的值
+def newton(k, x, fx, x1, c, nt):
+    nt.append(c[0][0])
+    for i in range(1, k):
+        tmp = c[i][i]
+        for j in range(i - 1, -1, -1):
+            tmp *= (x1 - x[j])
+        nt.append(nt[i - 1] + tmp)
+
+# 输出差商表和牛顿插值多项式的值
+def show(k, c, nt, x1):
+    for i in range(0, k):
+        for j in range(0, i + 1):
+            print("c[%d][%d]=%0.5lf" %(i, j, c[i][j]), end = " ")
+        print()
+    print()
+    for i in range(0, k):
+        print("N_%d<%g>=%0.5lf" %(i, x1, nt[i]))
+
+def main():
+    c = []
+    nt = []
+    x = [0.4, 0.55, 0.65, 0.80, 0.90]
+    fx = [0.41075, 0.57815, 0.69675, 0.88811, 1.02652]
+    x1 = 0.596
+    k = len(x)
+    get_diff_quo(k, x, fx, c)
+    newton(k, x, fx, x1, c, nt)
+    show(k, c, nt, x1)
+
+if __name__ == "__main__":
+    main()
---
+++ Numerical_Analysis/Romberg.py
@@ -0,0 +1,59 @@
+import math
+
+EPS = 1e-5
+
+def f1(x):
+    return math.exp(x) * math.cos(x)
+
+def f2(x):
+    return 4 / (1 + x * x)
+
+# 龙贝格算法的实现
+def romberg(f, a, b, T):
+    T.append([])
+    h = b - a # 步长
+    i = 0
+    err = 999.0
+    n = 1 # 区间数量
+    T[0].append((f(a) + f(b)) * h / 2)
+
+    while err >= EPS: # 当误差足够小，即小于预定的误差时结束计算，否则区间数量翻倍
+        s = 0
+        i += 1
+        for j in range(0, n):
+            s += f(a + (j+0.5) * h)
+        T.append([0.5 * T[i-1][0] + h / 2 * s])
+        for j in range(1, i + 1):
+            T[i].append((4**j * T[i][j-1] - T[i-1][j-1]) / (4**j-1)) # 龙贝格迭代公式
+        err = abs(T[i][i-1] - T[i-1][i-1])
+        h /= 2 # 步长减半
+        n *= 2 # 区间数量翻倍
+
+    print("#interval=%d, err=%e, res=%0.6lf\n" %(n, err, T[i][i]))
+
+# 用于输出外推结果表格
+def show(T):
+    print("区间数\t", end = " ")
+    for i in range (0, len(T)):
+        print("%d次外推\t" %i, end = " ")
+    print()
+    for i in range(0, len(T)):
+        print("----------------", end = "")
+    print()
+    for i in range(0, len(T)):
+        print(2**i, end = "\t")
+        for j in range(0, i+1):
+            print("%0.6lf\t" %T[i][j], end = "")
+        print()
+
+def main():
+    T = []
+    W = []
+    romberg(f1, 0, math.pi, T)
+    show(T)
+    print()
+    romberg(f2, 0, 1, W)
+    show(W)
+
+if __name__ == "__main__":
+    main()
