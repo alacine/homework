ce2354092778e85e6c65d3aa6511c13c703e2ea0
RyanAlacine <ryanruirr@gmail.com>
2018-05-23 01:31:17 +0800

    finish Gauss

--- Numerical_Analysis/Gauss.py
+++ Numerical_Analysis/Gauss.py
@@ -1,68 +1,77 @@
 # 高斯列主元消去法
 import copy
 
+# 输出列主元消去过程(取消17行和28行的注释)
 def show_c(c):
     for i in c:
         for j in i:
-            print("%10.3lf" % j, end = " ")
+            print("\t%g" % j, end = " ")
         print()
     print()
 
+# 高斯列主元消去算法
 def Guass(a, b, x):
     c = copy.deepcopy(a)
     for i in range(0, len(b)):
         c[i].append(b[i])
-    show_c(c)
+    # show_c(c)
 
+    # 列主元消去
     for i in range(0, len(c)-1):
-        c.sort(key = lambda x: abs(x[i in range(0, len(c))]), reverse = True)
+        # 排序，按照主元绝对值排序
+        c[i:] = sorted(c[i:], key = lambda x: abs(x[i]), reverse = True)
+        # 消元
         for j in range(i+1, len(c)):
             m = - c[j][i] / c[i][i]
             for k in range(0, len(c[j])):
-                c[j][k] += m * c[i][k]    
-        show_c(c)
-    show_c(c)
+                c[j][k] += m * c[i][k]
+        # show_c(c)
 
+    # 回带求值
+    for i in range(0, len(c)-1):
+        x.append(0)
     for i in range(len(c)-1, -1, -1):
         if i == len(c) - 1:
             x.append(c[i][len(c[i])-1] / c[i][i])
         else:
-            x.insert(0, c[i][len(c[i])-1])
-            for j in range(len(x)-1, 0, -1):
-                x[0] -= c[i][i+j] * x[j]
-            x[0] /= c[i][j]
-    print(x)
+            x[i] = c[i][len(c[i])-1]
+            for j in range(len(x)-1, i, -1):
+                x[i] -= c[i][j] * x[j]
+            x[i] /= c[i][j-1]
+    # 返回答案列表
+    return c
 
-def show(a, b, x):
+# 按要求格式化输出结果
+def show_answer(c, x):
     print("A")
-    for i in range(0, len(a)):
-        for j in range(0, len(a[i])):
-            print("\t", a[i][j], end = "")
+    for i in range(0, len(c)):
+        for j in range(0, len(c[i])-1):
+            print("\t%0.2lf" % c[i][j], end = "")
         print()
     print("b")
-    for i in range(0, len(b)):
-        print("%g\t" % b[i], end = "")
+    for i in range(0, len(c)):
+        print("\t%0.2lf" % c[i][len(c)-1], end = "")
     print("\nx")
     for i in range(0, len(x)):
-        print("%g\t" % x[i], end = "")
-    print()
+        print("\t%0.2lf" % x[i], end = "")
+    print("\n")
 
 def main():
     A = [[1, 2, 1], [2, 2, 3], [-1, -3, 0]]
     b = [0, 3, 2]
     x = []
+    c = Guass(A, b, x)
+    show_answer(c, x)
     A1 = [[0, 2, 1], [1, 1, 0], [2, 3, 2]]
     b1 = [5, 3, 0]
     x1 = []
+    c1 = Guass(A1, b1, x1)
+    show_answer(c1, x1)
     A2 = [[2, 1, 0, 0], [1, 3, 1, 0], [0, 1, 1, 1], [0, 0, 2, 1]]
     b2 = [1, 2, 2, 0]
     x2 = []
-    Guass(A, b, x)
-    show(A, b, x)
-    Guass(A1, b1, x1)
-    show(A1, b1, x1)
-    Guass(A2, b2, x2)
-    show(A2, b2, x2)
+    c2 = Guass(A2, b2, x2)
+    show_answer(c2, x2)
 
 if __name__ == "__main__":
     main()
