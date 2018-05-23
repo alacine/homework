425bd4b765addf2c8b28cb7062da4fefca892550
RyanAlacine <ryanruirr@gmail.com>
2018-05-24 00:15:56 +0800

    add Guass and LeastSquaresMethod

--- Numerical_Analysis/Gauss.py
+++ Numerical_Analysis/Gauss.py
@@ -1,19 +1,22 @@
 # 高斯列主元消去法
 import copy
 
-# 输出列主元消去过程(取消17行和28行的注释)
+# 输出列主元消去过程(取消20行和31行的注释)
 def show_c(c):
     for i in c:
         for j in i:
-            print('\t%g' % j, end = ' ')
+            print('\t%0.3g' % j, end = ' ')
         print()
     print()
 
-# 高斯列主元消去算法
-def Guass(a, b, x):
+def merge(a, b):
     c = copy.deepcopy(a)
     for i in range(0, len(b)):
         c[i].append(b[i])
+    return c
+
+# 高斯列主元消去算法
+def guass(c):
     # show_c(c)
 
     # 列主元消去
@@ -37,39 +40,41 @@ def Guass(a, b, x):
             for j in range(len(x)-1, i, -1):
                 x[i] -= c[i][j] * x[j]
             x[i] /= c[i][j-1]
-    # 返回答案列表
-    return c
+    # 返回解列表
+    return x
 
 # 按要求格式化输出结果
 def show_answer(c, x):
     print('A')
     for i in range(0, len(c)):
         for j in range(0, len(c[i])-1):
-            print('\t%0.2lf' % c[i][j], end = '')
+            print('\t%.3g' % c[i][j], end = '')
         print()
     print('b')
     for i in range(0, len(c)):
-        print('\t%0.2lf' % c[i][len(c)-1], end = '')
+        print('\t%.3g' % c[i][len(c[i])-1], end = '')
     print('\nx')
     for i in range(0, len(x)):
-        print('\t%0.2lf' % x[i], end = '')
+        print('\t%.3g' % x[i], end = '')
     print('\n')
 
 def main():
     A = [[1, 2, 1], [2, 2, 3], [-1, -3, 0]]
     b = [0, 3, 2]
-    x = []
-    c = Guass(A, b, x)
+    c = merge(A, b)
+    x = guass(c)
     show_answer(c, x)
+
     A1 = [[0, 2, 1], [1, 1, 0], [2, 3, 2]]
     b1 = [5, 3, 0]
-    x1 = []
-    c1 = Guass(A1, b1, x1)
+    c1 = merge(A1, b1)
+    x1 = guass(c1)
     show_answer(c1, x1)
+
     A2 = [[2, 1, 0, 0], [1, 3, 1, 0], [0, 1, 1, 1], [0, 0, 2, 1]]
     b2 = [1, 2, 2, 0]
-    x2 = []
-    c2 = Guass(A2, b2, x2)
+    c2 = merge(A2, b2)
+    x2 = guass(c2)
     show_answer(c2, x2)
 
 if __name__ == '__main__':
---
+++ Numerical_Analysis/LeastSquaresMethod.py
@@ -0,0 +1,45 @@
+# 数据拟合的最小二乘方法的实现 (Least Squares Method)
+from Gauss import guass, merge, show_c
+
+# 内积
+def dot_product(old_list, n):
+    new_list = [i**n for i in old_list]
+    return new_list
+
+# 向量相乘
+def product(la, lb):
+    sum = 0
+    for i, j in zip(la, lb):
+        sum += i * j
+    return sum
+
+# 根据所求多项式次数转化为相应大小的矩阵
+def transform(x, y, n):
+    c = [[] for i in range(0, n+1)]
+    for i in range(0, n+1):
+        c[i] = [product(dot_product(x, i), dot_product(x, j)) for j in range(0, n+1)]
+        c[i].append(product(dot_product(x, i), y))
+    return c
+
+# 输出得到的多项式
+def show_least_squares(x):
+    print('%g' % x[0], end = '')
+    for i in range(1, len(x)):
+        if x[i] >= 0:
+            print('+', end = '')
+        print('%gx' % x[i], end = '')
+        if i >= 2:
+            print('^%d' % i, end = '')
+    print()
+
+def main():
+    x = [0, 0.15, 0.31, 0.5, 0.6, 0.75]
+    y = [1.0, 1.004, 1.031, 1.117, 1.223, 1.422]
+    c = transform(x, y, 1)
+    # guaa(c) 用高斯列主元消去法解方程
+    show_least_squares(guass(c))
+    d = transform(x, y, 2)
+    show_least_squares(guass(d))
+
+if __name__ == '__main__':
+    main()
