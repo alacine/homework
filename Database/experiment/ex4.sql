7df29e0efc572160a47e0f820c08dee130b63acf
302-020 <302-020@SUNMNET.COM>
2018-06-08 11:52:18 +0800

    add ex4

---
+++ Database/experiment/ex4.sql
@@ -0,0 +1,22 @@
+insert into branch values('Downtown','Brooklyn','900000');
+insert into branch values('Redwood','Palo Atto','2100000');
+insert into branch values('Perryridge','Horseneck','1700000');
+insert into branch values('Mianus','Horseneck','400200');
+insert into branch values('Round Hill','Horseneck','8000000');
+insert into branch values('Pownal','Bennington','400000');
+insert into branch values('North Town','Rye','3700000');
+insert into branch values('Brighton','Brooklyn','7000000');
+insert into branch values('Central','Rye','400280');
+
+('Jones','Main','Harrison');
+('Smith','Main','Rye');
+'Hare','Main','Harrison'
+'Curry','North','Rye'
+'Lindsay','Park','Pittsfield'
+'Turner','Putnam','Stamford'
+'Williams','Nassau','Priceton'
+'Adams','Spring','Pittsfield'
+
+
+
+
