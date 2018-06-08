bd92950c82b355f8f308e97ec6ab9420efa77fa4
302-020 <302-020@SUNMNET.COM>
2018-06-08 11:11:02 +0800

    finish ex1 and ex2

---
+++ Database/experiment/delet.sql
@@ -0,0 +1,6 @@
+drop table account;
+drop table borrower;
+drop table branch;
+drop table customer;
+drop table depositor;
+drop table loan;
--- Database/experiment/ex2.sql
+++ Database/experiment/ex2.sql
@@ -3,40 +3,40 @@ create table branch (
     -- 分行表
     branch_name varchar2(20) not null,
     branch_city varchar2(30) not null,
-    assets integer,
+    assets integer
 );
 
 create table loan (
     -- 借款表
-    loan_number varchar2(10) not null, -- 借款号
+    loan_number varchar2(10) not null,
     branch_name varchar2(20) not null,
-    amount integer not null, -- 借款额
+    amount integer not null
 );
 
 create table account (
     -- 存款表
     account_number varchar2(10) not null,
     branch_name varchar2(20) not null,
-    balance integer not null,
+    balance integer not null
 );
 
 create table customer (
     -- 客户表
     customer_name varchar2(20) not null,
     customer_city varchar2(20),
-    customer_street varchar2(30),
+    customer_street varchar2(30)
 );
 
 create table borrower (
     -- 借款人表
     customer_name varchar2(20) not null,
-    loan_number varchar2(10) not null,
+    loan_number varchar2(10) not null
 );
 
 create table depositor (
     -- 存款人表
     customer_name varchar2(20) not null,
-    account_number varchar2(10) not null,
+    account_number varchar2(10) not null
 );
 
 
