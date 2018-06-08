a456772da99dafcb39e4d822326f7c30ee8197f3
RyanAlacine <ryanruirr@gmail.com>
2018-06-08 12:47:47 +0800

    add ex2 in 8086

---
+++ 8086/ex2.asm
@@ -0,0 +1,34 @@
+DATAS SEGMENT
+    a db 0ah,'end$'
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    
+    ;流程控制loop循环输出大写字母
+    mov dl,40h
+    mov cx,100
+s1: add dl,1
+    cmp dl,5ah
+    jg s2
+    mov ah,02h
+    int 21h
+    loop s1
+    
+s2: lea dx,a
+    mov ah,09h
+    int 21h
+    jmp s3
+    
+s3: MOV AH,4CH
+    INT 21H
+    
+CODES ENDS
+    END START
