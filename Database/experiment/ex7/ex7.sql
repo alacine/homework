5e58657eab90cb82f22fce26c64588609a64dbd4
RyanAlacine <ryanruirr@gmail.com>
2018-06-16 21:02:02 +0800

    finish ex7

--- Database/experiment/ex7/ex7.sql
+++ Database/experiment/ex7/ex7.sql
@@ -2,24 +2,28 @@
 update loan
 set amount = 1200
 where loan_number = 'L-11';
+commit;
 
 --2
 update loan
 set branch_name = 'Perryridge', amount = 1400
 where loan_number = 'L-20';
+commit;
 
 --3
 update account
 set balance = balance * 1.06
-where account_number = (select account_number
+where account_number in (select account_number
                         from depositor
                         where customer_name = 'Smith');
+commit;
 
 --4
 update account
 set balance = case when balance > 2000 then balance * 1.06
                    else balance * 1.05
-              end
+              end;
+commit;
 
 --5
 update customer
@@ -27,19 +31,23 @@ set customer_city = (select customer_city
                      from customer
                      where customer_name = 'Jones')
 where customer_name = 'Smith';
+commit;
 
 --6
-with temp(customer_city, customer_street) as
-    (select customer_city, customer_street
-     from customer
-     where customer_name = Turner)
 update customer
-set customer_city = temp.customer_city, customer_street = temp.customer_street
+set customer_city = (select customer_city
+                     from customer
+                     where customer_name = 'Turner'),
+    customer_street = (select customer_street
+                       from customer
+                       where customer_name = 'Turner')
 where customer_name = 'Smith';
+commit;
 
 --7
 delete from customer
 where customer_name = 'Brooks';
+commit;
 
 --8
 delete from customer
@@ -48,3 +56,17 @@ where customer_name not in ((select customer_name
                             union
                             (select customer_name
                              from depositor));
+commit;
+
+--9
+select *
+from loan;
+
+select *
+from account;
+
+select *
+from customer;
+
+--10
+rollback;
