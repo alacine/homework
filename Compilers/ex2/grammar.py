bdc9405daa40ed15361e1e9a182b150ac27a67b2
alacine <ryanruirr@gmail.com>
2018-12-11 15:50:53 +0800

    update ex2

--- Compilers/ex2/grammar.py
+++ Compilers/ex2/grammar.py
@@ -11,22 +11,26 @@ class Grammar:
         print('Vt: ', self.vt)
         print('Vn: ', self.vn)
         print('S: ', self.s)
+        print('xi: ')
         for key in self.xi:
             print('\t', key, '->', self.xi[key])
-        return '---------------------------'
+        return '--------------------------'
 
     def read(self):
         self.vt = list(input('输入终结符集合(要求: 1.小写字母或数字或符号 2.不要有空格和|)\n'))
-        self.vn = list(input('输入非终结符集合, 第一个默认作为文法开始符(要求: 1.大写字母且 2.不能有空格)\n'))
+        self.vn = list(input('输入非终结符集合, 第一个默认作为文法开始符(要求: 1.大写字母 2.不能有空格)\n'))
+        self.s = self.vn[0]
         print('输入产生式, 每行一个, 例如(S ET)表示S->ET')
         while True:
             in_xi = input().split()
             if not in_xi:
                 break
             if in_xi[0] in self.xi:
-                self.xi[in_xi[0]].append(in_xi[1])
+                self.xi[in_xi[0]] += (' ' + in_xi[1])
             else:
-                self.xi[]
+                self.xi[in_xi[0]] = in_xi[1]
+        for key in self.xi:
+            self.xi[key] = self.xi[key].split()
 
 def main():
     g = Grammar()
